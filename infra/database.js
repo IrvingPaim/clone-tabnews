21062b453f7936f18ed4cb82e6feb3e21479b235
Irving Paim <101907033+IrvingPaim@users.noreply.github.com>
2025-01-17 23:34:36 +0000

    test: "Test jest Fixed"

--- infra/database.js
+++ infra/database.js
@@ -23,7 +23,7 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
+    ssl: true, //getSSLValues(),
   });
 
   await client.connect();
@@ -37,12 +37,12 @@ const database = {
 
 export default database;
 
-function getSSLValues() {
-  if (process.env.POSTGRES_CA) {
-    return {
-      ca: process.env.POSTGRES_CA,
-    };
-  }
+// function getSSLValues() {
+//   if (process.env.POSTGRES_CA) {
+//     return {
+//       ca: process.env.POSTGRES_CA,
+//     };
+//   }
 
-  return process.env.NODE_ENV === "production" ? true : false;
-}
+//   return process.env.NODE_ENV === "production" ? true : false;
+// }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,6 +2,7 @@ import orchestrator from "../orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
+  //await orchestrator.clearDatabase();
 });
 
 describe("GET /api/v1/status", () => {
@@ -17,7 +18,7 @@ describe("GET /api/v1/status", () => {
       const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
       expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
-      expect(responseBody.dependencies.database.version).toEqual("16.5");
+      expect(responseBody.dependencies.database.version).toEqual("16.6");
       expect(responseBody.dependencies.database.max_connections).toEqual(112);
       expect(responseBody.dependencies.database.opened_connections).toEqual(1);
     });
