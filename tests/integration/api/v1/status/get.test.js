898c53bbc092602460e368ebbb2eb9da11587ad9
Irving Paim <101907033+IrvingPaim@users.noreply.github.com>
2025-03-29 00:41:55 +0000

    test: change max_connections to 901

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -19,7 +19,7 @@ describe("GET /api/v1/status", () => {
       expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
       expect(responseBody.dependencies.database.version).toEqual("16.8");
-      expect(responseBody.dependencies.database.max_connections).toEqual(112);
+      expect(responseBody.dependencies.database.max_connections).toEqual(901);
       expect(responseBody.dependencies.database.opened_connections).toEqual(1);
     });
   });
