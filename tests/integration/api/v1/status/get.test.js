487ae07b8782fed243e0ca8bb1242ecbc17f31db
Irving Paim <101907033+IrvingPaim@users.noreply.github.com>
2025-06-23 14:29:03 +0000

    fix: erro no get status

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -18,7 +18,7 @@ describe("GET /api/v1/status", () => {
       const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
       expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
-      expect(responseBody.dependencies.database.version).toEqual("16.8");
+      expect(responseBody.dependencies.database.version).toEqual("16.9");
       expect(responseBody.dependencies.database.max_connections).toEqual(901);
       expect(responseBody.dependencies.database.opened_connections).toEqual(1);
     });
